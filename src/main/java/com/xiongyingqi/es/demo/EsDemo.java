bb10965bafece00c8248e715522fb735f8d5b2a3
unknown <>
2016-08-20 20:12:27 +0800

    simp

--- src/main/java/com/xiongyingqi/es/demo/EsDemo.java
+++ src/main/java/com/xiongyingqi/es/demo/EsDemo.java
@@ -4,8 +4,8 @@ import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.junit.Assert;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -16,30 +16,32 @@ import java.util.Map;
  * Created by qi on 16-8-10.
  */
 public class EsDemo {
-    public static void main(String[] args) throws UnknownHostException {
-        // on startup
-        Client client = TransportClient.builder().build()
+  public static void main(String[] args) throws UnknownHostException {
+    // on startup
+    Client client = TransportClient.builder()
+        .settings(Settings.settingsBuilder().put("cluster.name", "elasticsearch"))
+        .build()
 //                .addTransportAddress(
-//                        new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9301))
-                .addTransportAddress(
-                        new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
+//                        new InetSocketTransportAddress(InetAddress.getByName("192.168.56.101"),
+//                            9300));
+        .addTransportAddress(
+            new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
 
-        Map<String, Object> json = new HashMap<String, Object>();
-        json.put("user", "kimchy");
-        json.put("postDate", "2016-08-08");
-        json.put("message", "trying out Elasticsearch");
-        IndexResponse indexResponse = client.prepareIndex("test", "test", "1")
-                .setSource(json)
-                .get();
-        System.out.println("indexResponse====" + indexResponse);
+    Map<String, Object> json = new HashMap<String, Object>();
+    json.put("user", "kimchy");
+    json.put("postDate", "2016-08-08");
+    json.put("message", "trying out Elasticsearch");
+    IndexResponse indexResponse = client.prepareIndex("test", "test", "1")
+        .setSource(json)
+        .get();
+    System.out.println("indexResponse====" + indexResponse);
 
-        GetResponse response = client.prepareGet("test", "test", "1")
-                .setOperationThreaded(false)
-                .get();
+    GetResponse response = client.prepareGet("test", "test", "1")
+        .setOperationThreaded(false)
+        .get();
 
-        Assert.assertEquals(json, response.getSource());
-        System.out.println("get response=========" + response.getSource());
-        // on shutdown
-        client.close();
-    }
+    System.out.println("get response=========" + response.getSource());
+    // on shutdown
+    client.close();
+  }
 }
